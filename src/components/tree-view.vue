c9e24c9cfaeea7355594001979ecd7ce71fbb999
Shrinath Prabhu <shrinath.prabhu@hashbinary.com>
2020-10-14 16:39:16 +0530

    Added children checkbox changes

--- src/components/tree-view.vue
+++ src/components/tree-view.vue
@@ -9,19 +9,24 @@
             @click.stop="toggle(index)"
             >{{ expansions[index].symbol }}</span
           >
-          <span v-else style="margin-right: 15px">&nbsp;</span>
+          <span v-else style="margin-right: 14px"></span>
           <input
             class="children-selected"
             type="checkbox"
             :id="item.source + index"
+            v-model="expansions[index].selection"
+            @change="notifyParent(expansions[index].selection)"
           />
           <label :for="item.source + index">
             {{ item.source }}
           </label>
         </div>
         <tree-view
-          v-show="expansions[index].value"
+          v-if="expansions[index].value"
           :items="item.children"
+          :selectAllChildren="expansions[index].seletion"
+          :parentNode="item.source + index"
+          @notify="notification"
         ></tree-view>
       </li>
     </ul>
@@ -41,17 +46,12 @@ input,
 label {
   cursor: pointer;
 }
-
-.children-selected:checked::after {
-  content: \f054;
-  color: white;
-}
 </style>
 
 <script>
 export default {
   name: "tree-view",
-  props: ["items"],
+  props: ["items", "selectAllChildren", "parentNode"],
   data: () => ({
     expansions: [],
     dataPopulated: false,
@@ -60,9 +60,36 @@ export default {
   methods: {
     toggle(index) {
       this.$set(this.expansions, index, {
-        symbol: this.expansions[index].symbol === "+" ? "-" : "+",
+        symbol: this.expansions[index].symbol === "+" ? "−" : "+",
         value: !this.expansions[index].value,
+        selection: this.expansions[index].selection,
       });
+    },
+
+    notifyParent(selection) {
+      let index = this.expansions.findIndex(
+        (e) => e.selection == false || e.selection == undefined
+      );
+      let selectedAll = index === -1 ? true : false;
+      this.$emit("notify", {
+        parentNode: this.parentNode,
+        selection,
+        selectedAll,
+      });
+    },
+
+    notification(e) {
+      if (e.selectedAll) {
+        document.getElementById(e.parentNode).checked = true;
+        this.notifyParent(true);
+      } else if (e.selection) {
+        document.getElementById(e.parentNode).indeterminate = true;
+        this.notifyParent(true);
+      } else {
+        document.getElementById(e.parentNode).checked = false;
+        document.getElementById(e.parentNode).indeterminate = false;
+        this.notifyParent(false);
+      }
     },
   },
   mounted() {
@@ -71,9 +98,17 @@ export default {
       this.$set(this.expansions, index, {
         symbol: "+",
         value: false,
+        selection: this.selectAllChildren
+          ? Boolean(this.selectAllChildren)
+          : false,
       });
     });
     this.dataPopulated = true;
+  },
+  watch: {
+    selectAllChildren: function (newVal) {
+      console.log("Changes happend", newVal, this.items.length);
+    },
   },
 };
 </script>
