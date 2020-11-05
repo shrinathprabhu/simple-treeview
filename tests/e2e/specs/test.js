957eabf38973a2a0d8a9827c1883f94d37b8b9c3
Shrinath Prabhu <shrinath.prabhu@hashbinary.com>
2020-11-05 16:42:35 +0530

    modified unit test case

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,12 +1,13 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('E2E testing', done => {
-  it('Contains ul, .clickable and checkbox after data loads', () => {
-    cy.visit('/');
+describe("E2E testing", (done) => {
+  it("Contains ul, .clickable and checkbox after data loads", () => {
+    cy.visit("/");
     setTimeout(() => {
-      cy.contains('ul');
-      cy.contains('.clickable');
-      cy.contains('input[type=checkbox]');
+      cy.contains("ul");
+      cy.contains(".clickable");
+      cy.contains("input[type=checkbox]");
+      done();
     }, 3000);
   });
 });
